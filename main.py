34e49e3f270fea19ef02709752c1ecdce2d16cdf
Richard Mahn <richmahn@users.noreply.github.com>
2022-06-02 12:06:07 -0600

    Fixes getenv

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from os import getenv
 from ts_v2_catalog_backport.ts_v2_catalog_backport import TsV2CatalogBackporter
 
 if __name__ == '__main__':
-    debug = getenv('DEBUG', True).lower() in ('true', '1', 't')
+    debug = getenv('DEBUG', 'True').lower() not in ('false', '0', 'f', '')
     backporter = TsV2CatalogBackporter(prefix=getenv('PREFIX', 'dev-'), debug=debug)
     backporter.run()
